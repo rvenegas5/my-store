4d0ce3e9b8e8e6ad8594280795a2d4126a6a35ce
William Venegas <rvenegas@espol.edu.ec>
2021-10-29 16:56:12 -0500

    Learning Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,22 @@ export class AppComponent {
   title = 'my-store';
   btnDisabled: boolean = false;
   textExample: string = "Example..."
+  count: number = 0;
+  progress: number = 0;
+  warning!: string;
+
+  onClick(): void {
+    this.count = this.count + 1;
+  }
+
+  clearCounter(): void {
+    this.count = 0;
+  }
+
+  incrementProgress(): void {
+    if (this.progress === 100)
+      this.warning = "The max increment is 100%";
+    else
+      this.progress = this.progress + 2;
+  }
 }
