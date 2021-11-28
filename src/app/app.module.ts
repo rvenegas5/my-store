864c00469bf1cee07a6e138d6588c306afe54b3a
William Venegas <rvenegas@espol.edu.ec>
2021-11-28 09:59:18 -0500

    Img component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './_register/register/register.component';
-
+import { ImgComponent } from './components/img/img.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    ImgComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/components/img/img.component.spec.ts
+++ src/app/components/img/img.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegisterComponent } from './register.component';
+import { ImgComponent } from './img.component';
 
-describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+describe('ImgComponent', () => {
+  let component: ImgComponent;
+  let fixture: ComponentFixture<ImgComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ ImgComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegisterComponent);
+    fixture = TestBed.createComponent(ImgComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
