dfb8a8c9c4ef9a274f92c3d91d055d6093ae3836
William Venegas <rvenegas@espol.edu.ec>
2021-12-05 22:46:16 -0500

    Adding products cards and using the Input decorator to data flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImgComponent,
-
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
